8106bfd1113e5010232dd8852e78d02689c792a6
Sahil Thakur <thakursahil1912@gmail.com>
2024-05-07 20:57:31 +0530

    updates

--- 03tailwindprops/components/Card.jsx
+++ 03tailwindprops/components/Card.jsx
@@ -4,12 +4,28 @@ import React from 'react'
 function Card() {
   return (
     <div>
-        <img  src="https://cdn.pixabay.com/photo/2016/12/16/15/25/christmas-1911637_1280.jpg" alt="" />
-        <h1 className='text-3xl bg-green-500 p-3 rounded-md'>Card for photos</h1>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet 
-            corporis qui suscipit obcaecati dicta vitae pariatur ea delectus
-            rem sunt, enim commodi iure velit id dolore distinctio fugit beatae</p>
+       <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+  <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="https://cdn.pixabay.com/photo/2018/08/23/07/35/thunderstorm-3625405_1280.jpg" alt="" width="384" height="512"/>
+  <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
+    <blockquote>
+      <p className="text-lg font-medium">
+       Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit, laudantium sint expedita corrupti quos 
+       reprehenderit delectus nesciunt aperiam explicabo modi ratione at fugiat temporibus. Minus ipsum 
+       consequuntur perspiciatis? Explicabo, est.
+      </p>
+    </blockquote>
+    <figcaption className="font-medium">
+      <div className="text-sky-500 dark:text-sky-400">
+        Sarah Dayan
+      </div>
+      <div className="text-slate-700 dark:text-slate-500">
+        Staff Engineer, Algolia
+      </div>
+    </figcaption>
+  </div>
+</figure>
     </div>
+    
   )
 }
 
--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -6,6 +6,9 @@ function App() {
     <>
       <h1 className= 'text-3xl bg-green-500 p-3 rounded-md '>Vite with tailwind</h1>
       <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
     </>
   )
 }
