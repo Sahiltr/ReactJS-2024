16f68ac106adc0a7b1aca8a5d6eb4680983c96cc
Sahil Thakur <thakursahil1912@gmail.com>
2024-05-07 21:58:58 +0530

    tailwind

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+import Card from './Components/Card.jsx'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl bg-green-500 p-3'>tailwind css with vite</h1>
+      <Card/>
+      <br />
+      <Card/>
+      <br />
+      <Card/>
+      <br />
+      <Card/>
+    </>
+  )
+}
+
+export default App
